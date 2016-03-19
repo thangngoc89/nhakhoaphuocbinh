d34f0882899241dd0922b459050736b3ff6bf8b6
Khoa Nguyen <hi@khoanguyen.me>
2016-03-19 19:21:44 +0700

    Fix https error

--- web_modules/LayoutContainer/index.js
+++ web_modules/LayoutContainer/index.js
@@ -28,11 +28,11 @@ export default class Layout extends Component {
           link={ [
             {
               "rel": "stylesheet",
-              "href": "http://yui.yahooapis.com/pure/0.6.0/pure-min.css",
+              "href": "//yui.yahooapis.com/pure/0.6.0/pure-min.css",
             },
             {
               "rel": "stylesheet",
-              "href": "http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css",
+              "href": "//yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css",
             },
           ] }
           meta={ [
